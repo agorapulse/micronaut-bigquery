1359a5da8914798fc80a53661d66b511ea52e8cf
Vladimir Orany <vladimir@orany.cz>
2020-06-05 11:54:05 +0200

    running the bigquery tests only in branch tests

--- libs/micronaut-bigquery/src/test/groovy/com/agorapulse/micronaut/bigquery/DefaultBigQueryServiceSpec.groovy
+++ libs/micronaut-bigquery/src/test/groovy/com/agorapulse/micronaut/bigquery/DefaultBigQueryServiceSpec.groovy
@@ -2,7 +2,17 @@ package com.agorapulse.micronaut.bigquery
 
 import com.agorapulse.micronaut.bigquery.tck.BigQueryServiceSpec
 import io.micronaut.context.ApplicationContext
+import spock.lang.Requires
 
+@Requires({
+    System.getenv('GOOGLE_APPLICATION_CREDENTIALS') &&
+        (
+            !System.getenv('GITHUB_REF') ||
+            (
+                System.getenv('GITHUB_REF') && System.getenv('GITHUB_REF').startsWith('refs/heads/')
+            )
+        )
+})
 class DefaultBigQueryServiceSpec extends BigQueryServiceSpec {
 
     @Override
